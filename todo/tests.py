816c16235c0aa0bcadb5ce76117d4fba29014260
Ruslan Misko <miskoruslan6@gmail.com>
2023-11-27 15:26:02 +0200

    implement more tests for admin and forms

--- todo/tests.py
+++ todo/tests.py
@@ -1,6 +1,10 @@
-from django.test import TestCase
-from .models import Tag, Task
 from datetime import datetime, timedelta
+from django.utils import timezone
+from todo.forms import TaskForm, TagForm
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from todo.models import Task, Tag
 
 
 class TaskModelTest(TestCase):
@@ -37,3 +41,52 @@ class TaskModelTest(TestCase):
     def test_tags_associated_with_task(self):
         self.assertEqual(list(self.task1.tags.all()), [self.tag1])
         self.assertEqual(list(self.task2.tags.all()), [self.tag1, self.tag2])
+
+
+class TaskFormTest(TestCase):
+
+    def test_task_form_invalid_data(self):
+        form_data = {
+            'deadline': str(timezone.now()),
+            'is_done': False,
+        }
+        form = TaskForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class TagFormTest(TestCase):
+    def test_tag_form_valid_data(self):
+        form_data = {
+            'name': 'Test Tag',
+        }
+        form = TagForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_tag_form_invalid_data(self):
+        form_data = {}
+        form = TagForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
+class AdminPanelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user.is_staff = True
+        self.user.is_superuser = True
+        self.user.save()
+
+        self.client = Client()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_task_admin_panel(self):
+        task = Task.objects.create(content='Test Task', is_done=False)
+        url = reverse('admin:todo_task_change', args=[task.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_tag_admin_panel(self):
+        tag = Tag.objects.create(name='Test Tag')
+        url = reverse('admin:todo_tag_change', args=[tag.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
